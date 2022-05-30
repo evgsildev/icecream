d419e88ba17c23db0bc70d6643d9dc5d3954b998
Evgenii Silenko <evgen.silen@gmail.com>
2022-05-30 20:59:09 +0300

    form

--- js/modal.js
+++ js/modal.js
@@ -1,8 +1,25 @@
 (() => {
     const refs = {
+      openHeroBtn: document.querySelector('[data-hero-open]'),
+      closeHeroBtn: document.querySelector('[data-hero-close]'),
+      heroModal: document.querySelector('[data-hero]'),
+      
+    };
+  
+    refs.openHeroBtn.addEventListener('click', toggleModal);
+    refs.closeHeroBtn.addEventListener('click', toggleModal);
+  
+    function toggleModal() {
+      refs.heroModal.classList.toggle('is-hidden');
+    }
+  })();
+
+(() => {
+    const refs = {
       openModalBtn: document.querySelector('[data-modal-open]'),
       closeModalBtn: document.querySelector('[data-modal-close]'),
       modal: document.querySelector('[data-modal]'),
+
     };
   
     refs.openModalBtn.addEventListener('click', toggleModal);
@@ -10,5 +27,53 @@
   
     function toggleModal() {
       refs.modal.classList.toggle('is-hidden');
+    }
+  })();
+
+  (() => {
+    const refs = {
+      openFranchiseBtn: document.querySelector('[data-franchise-open]'),
+      closeFranchiseBtn: document.querySelector('[data-franchise-close]'),
+      franchiseModal: document.querySelector('[data-franchise]'),
+      
+    };
+  
+    refs.openFranchiseBtn.addEventListener('click', toggleModal);
+    refs.closeFranchiseBtn.addEventListener('click', toggleModal);
+  
+    function toggleModal() {
+      refs.franchiseModal.classList.toggle('is-hidden');
+    }
+  })();
+
+(() => {
+    const refs = {
+      openInfoBtn: document.querySelector('[data-info-open]'),
+      closeInfoBtn: document.querySelector('[data-info-close]'),
+      infoModal: document.querySelector('[data-info]'),
+      
+    };
+  
+    refs.openInfoBtn.addEventListener('click', toggleModal);
+    refs.closeInfoBtn.addEventListener('click', toggleModal);
+  
+    function toggleModal() {
+      refs.infoModal.classList.toggle('is-hidden');
+    }
+  })();
+
+(() => {
+    const refs = {
+      openFormBtn: document.querySelector('[data-form-open]'),
+      closeFormBtn: document.querySelector('[data-form-close]'),
+      formModal: document.querySelector('[data-form]'),
+      
+    };
+  
+    refs.openFormBtn.addEventListener('click', toggleModal);
+    refs.closeFormBtn.addEventListener('click', toggleModal);
+  
+    function toggleModal() {
+      refs.formModal.classList.toggle('is-hidden');
     }
   })();
