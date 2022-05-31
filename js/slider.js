f06dc0c1dd2a8eaa438b56c1139d9f54673e2f0c
Evgenii Silenko <evgen.silen@gmail.com>
2022-05-31 23:59:47 +0300

    silly-slider

---
+++ js/slider.js
@@ -0,0 +1,21 @@
+const tabsItem = document.querySelectorAll('.review__item');
+const tabsBtns = document.querySelectorAll('.icon-list__button');
+
+function hiddeTabs () {
+    tabsItem.forEach(item => item.classList.add('hidden'));
+    tabsBtns.forEach(item => item.classList.remove('is-click'));
+}
+
+function showTab(index) {
+    tabsItem[index].classList.remove('hidden');
+    tabsBtns[index].classList.add('is-click');
+}
+
+hiddeTabs (0);
+showTab(0);
+
+tabsBtns.forEach((btn, index) => btn.addEventListener('click', () => {
+    hiddeTabs();
+    showTab(index);
+}));
+
